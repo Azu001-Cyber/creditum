67a1cc199d473693525ca558a3ba02e5cc92f84d
cybercodemomo <cybercodemomo@gmail.com>
2025-11-20 12:40:39 +0100

    ninth commit to creditum: changes ii

--- creditum/creditum/settings.py
+++ creditum/creditum/settings.py
@@ -30,11 +30,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # ALLOWED_HOSTS = []
 
-# DEBUG =False
-
-
+# DEBUG = False
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
